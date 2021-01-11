e3a906d13643c54a18361c2281a1d5772af993a0
Przemyslaw Czuj <przczuj@gmail.com>
2021-01-11 23:59:02 +0100

    Add simple gcode file printing program

---
+++ print.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+DEVICE=${1:-/dev/ttyUSB0}
+BAUD=${2:-115200}
+
+function echot() {
+    echo "[$(date +%H:%M:%S)] $@"
+}
+
+fuser -k $DEVICE
+stty -F $DEVICE -echo $BAUD
+
+exec 3<> $DEVICE
+
+while read gcode_line; do
+    if [[ "$gcode_line" == ";"* ]]; then
+        echot "IGNR: $gcode_line"
+        continue
+    elif [ -n "$gcode_line" ]; then
+        echot "SEND: $gcode_line"
+        echo $gcode_line >&3
+    fi
+    while read -t 10 gcode_response <&3; do
+        RETURN_CODE=$?
+        echot "RECV: "$gcode_response
+        if [ $RETURN_CODE -gt 128 ] || [[ "$gcode_response" == "ok"* ]]; then
+            break
+        fi
+    done
+done
