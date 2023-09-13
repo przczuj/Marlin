4507ed16f07ae2e74b29bcc1b7f2fd440fc6e001
Przemyslaw Czuj <przczuj@gmail.com>
2023-09-13 11:37:36 +0200

    Calibrate Z axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -595,7 +595,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 82, 82, 390, 417.63 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 82, 82, 400, 417.63 }
 
 /**
  * Default Max Feed Rate (mm/s)
