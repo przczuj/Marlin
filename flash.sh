1ef23f177ae916363db9b86f9d4ef2eda164a9c3
Przemyslaw Czuj <przczuj@gmail.com>
2026-04-29 20:15:53 +0200

    Optimise flash script (requires new version of arduino-cli)

--- flash.sh
+++ flash.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-arduino-cli compile -b arduino:avr:mega -p /dev/ttyUSB0 -u -v Marlin
+arduino-cli compile \
+  -b arduino:avr:mega \
+  --build-path build/.arduino-build \
+  --build-cache-path build/.arduino-cache \
+  --jobs 4 \
+  -p /dev/ttyUSB0 -u -v Marlin
