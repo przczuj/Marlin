adc70da86d9b87668195ae1474ba575f637467de
Przemyslaw Czuj <przczuj@gmail.com>
2026-05-01 18:51:00 +0200

    Add gcode loop generator for flattening

---
+++ loop-square-gcode.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+MIN_X=10
+MAX_X=210
+MIN_Y=10
+MAX_Y=210
+
+# init
+echo "G90"
+echo "G28"
+
+while true; do
+    echo "G1 X$MIN_X Y$MIN_Y"
+    echo "G1 X$MAX_X Y$MIN_Y"
+    echo "G1 X$MAX_X Y$MAX_Y"
+    echo "G1 X$MIN_X Y$MAX_Y"
+done
